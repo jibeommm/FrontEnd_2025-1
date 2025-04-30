c3defbf96ce08ac7a1d86afb52644d0b53911837
jibeommm <hong2002jb@gmail.com>
2025-04-30 12:35:01 +0900

    4주차 제출

--- script.js
+++ script.js
@@ -1,31 +1,22 @@
 // /script.js
-async function searchCountry() {
-    const input = document.getElementById('countryInput').value.trim();
+async function search() {
+    const input = document.getElementById('input').value.trim();
     const resultDiv = document.getElementById('result');
     resultDiv.innerHTML = '';
   
-    if (!input) {
-      resultDiv.textContent = '나라명을 입력해주세요!';
-      return;
-    }
-  
     try {
-      const res = await fetch(`https://restcountries.com/v3.1/name/${input}`);
-      if (!res.ok) throw new Error('찾을 수 없음');
-  
-      const data = await res.json();
-      const country = data[0];
-  
-      const flag = country.flags?.png || '';
-      const capital = country.capital?.[0] || '정보 없음';
-  
-      resultDiv.innerHTML = `
-        <h2>${country.name.common}</h2>
-        <p><strong>수도:</strong> ${capital}</p>
-        <img src="${flag}" alt="국기">
-      `;
+        const RestAPI = await fetch(`https://restcountries.com/v3.1/name/${input}`);
+        const data = await RestAPI.json();
+        const country = data[0];
+        const capital = country.capital;
+        const flag = country.flags.png;
+        resultDiv.innerHTML = `
+            <h2>나라 : ${input}</h2>
+            <p>수도 : ${capital}</p>
+            <img src="${flag}">
+        `;
     } catch (error) {
-      resultDiv.textContent = '국가 정보를 찾을 수 없습니다 🥲';
+      resultDiv.textContent = '...';
     }
 }
   
